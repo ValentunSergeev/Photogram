adc1b86a5929ff29ddf2b3f1a839d2eafc3ce1eb
Valentun <karbon4ic@gmail.com>
2016-07-01 10:47:34 +0600

    add alert

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,10 @@ class PostsController < ApplicationController
 
   def create
     if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
   end
@@ -24,14 +26,19 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to post_path(@post)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
     else
+      flash.now[:alert] = "Update failed.  Please check the form."
       render :edit
+    end
   end
+
 
   def destroy
     @post.destroy
     redirect_to posts_path
+    flash[:success] = "Post has been deleted."
   end
 
   private
